c7789c119eda3352eb5ff0aeb96a24d105057dcd
skyseo <22100370@handong.ac.kr>
2022-09-10 18:38:56 +0900

    7th commit

--- WordMasterProject_22100370/src/com/mycom/word/WordManager.java
+++ WordMasterProject_22100370/src/com/mycom/word/WordManager.java
@@ -53,6 +53,12 @@ public class WordManager {
 			else if(menu == 1) {
 				wordCRUD.listAll();
 			}
+			else if(menu == 2) {
+				wordCRUD.searchLevel();
+			}
+			else if(menu == 3) {
+				wordCRUD.searchWord();
+			}
 			else if(menu == 5) {
 				wordCRUD.updateItem();
 			}
